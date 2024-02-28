fe7efc30aec9ba781aa4de44d01d6895aa7ed8ec
vhvalbone <albin06svensson@gmail.com>
2024-02-28 10:13:27 +0100

    Add project files.

---
+++ inlämning 6.10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string text = "5 4,1 hej 9,04";
+        int antalTal = AntalTalIText(text);
+        Console.WriteLine("Antal tal i texten: " + antalTal);
+    }
+
+    public static int AntalTalIText(string text)
+    {
+
+        string[] ord = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        int antalTal = 0;
+        foreach (var ordet in ord)
+        {
+
+            if (double.TryParse(ordet, out double tal))
+            {
+                antalTal++;
+            }
+        }
+
+        return antalTal;
+    }
+}
